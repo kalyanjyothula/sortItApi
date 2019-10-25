570540bdc3f19daf38e81a66a6fe27ef5a6adb3a
kalyanjyothula <kalyanjyothula@gmail.com>
2019-10-25 16:16:47 +0530

    gitignore added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 // ------   public routes ---------
 
-app.post("/signup", (req, res) => {
+app.post("/api/signup", (req, res) => {
   const body = _.pick(req.body, ["email", "password", "mobileNo", "userName"]);
   const user = new User(body);
   user
@@ -23,19 +23,23 @@ app.post("/signup", (req, res) => {
       return data.generateAuthToken();
     })
     .then(token =>
-      res.header("x-auth", token).send({ _id: user._id, email: user.email })
+      res
+        .header("x-auth", token)
+        .send({ success: true, _id: user._id, email: user.email })
     )
     .catch(err => res.status(400).send(err));
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/login", (req, res) => {
   const body = _.pick(req.body, ["email", "password"]);
   User.findByCredentials(body.email, body.password)
     .then(user => {
       return user
         .generateAuthToken()
         .then(token =>
-          res.header("x-auth", token).send({ _id: user._id, email: user.email })
+          res
+            .header("x-auth", token)
+            .send({ success: true, _id: user._id, email: user.email })
         );
     })
     .catch(err => {
@@ -44,16 +48,16 @@ app.post("/login", (req, res) => {
     });
 });
 
-app.delete("/logout", authentication, (req, res) => {
+app.delete("/api/logout", authentication, (req, res) => {
   req.user
     .removeToken(req.token)
-    .then((data) => res.status(200).send())
+    .then(data => res.status(200).send())
     .catch(err => res.status(400).send());
 });
 
 //---- private route --------
 
-app.post("/cart", authentication, (req, res) => {
+app.post("/api/cart", authentication, (req, res) => {
   res.send(req.user);
 });
 
